9c05f704969a20d73cfc76f655c8edce70cc503f
primemix <monstrpro@gmail.com>
2017-10-12 23:34:05 +0300

    Added & use finder component

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
 require_once 'vendor/autoload.php';
+
+use Symfony\Component\Finder\Finder;
+
+$finder = new Finder();
+$finder->files()->in(__DIR__);
+
+foreach ($finder as $file) {
+    // Dump the absolute path
+    var_dump($file->getRealPath());
+
+    // Dump the relative path to the file, omitting the filename
+    var_dump($file->getRelativePath());
+
+    // Dump the relative path to the file
+    var_dump($file->getRelativePathname());
+}
+
